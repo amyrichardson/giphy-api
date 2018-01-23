68ca42c845b28f19b47540797d04587cb89a4b31
Amy Richardson <amyrichardson96@gmail.com>
2018-01-23 15:11:52 -0600

    random giphy working

--- server/public/scripts/controllers/random.controller.js
+++ server/public/scripts/controllers/random.controller.js
@@ -2,6 +2,12 @@ app.controller('RandomController', ['$http', function($http){
     console.log('RandomController loaded');
     const self = this;
 
-    
+    self.getRandom = function(){
+        let url = 'http://api.giphy.com/v1/gifs/random?api_key=e5EoJjbBgo5kIWE1H4dgFHuzeXr52REd';
+        $http.get(url).then(function (response) {
+            console.log('response;', response);
+            self.randomResult = response.data.data;
+        })
+    }
 
 }]);
